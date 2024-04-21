635babe134b009bb9dc0ff0bff925810af5e2f2a
kashishhh2004 <167668027+kashishhh2004@users.noreply.github.com>
2024-04-21 11:01:31 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+//fibonacci series generator
+
+def fibonacci():
+    x, y = 0, 1
+    while True:
+        yield x
+        x, y = y, x + y
+
+# Accept input from the user
+n = int(input("Input the number of Fibonacci numbers you want to generate? "))
+
+print("Number of first ",n,"Fibonacci numbers:")
+fib = fibonacci()
+for _ in range(n):
+    print(next(fib),end=" ")
